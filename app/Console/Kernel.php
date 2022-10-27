80a37022bd632ae606a686d56fb124627b582dd3
HUGO MUÑOZ <=>
2022-10-27 16:56:01 -0500

    se trabaja con la sincronizacion de stocks de puebla

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -122,9 +122,36 @@ class Kernel extends ConsoleKernel
         }      
     })->hourly();
    
-   
-   
-
+    $schedule->call(function (){
+        $workpoint = env("WORKPOINT");
+        $workpoint = env("WORKPOINT");
+        if($workpoint == 18){
+            $select = 
+            "SELECT F_ART.CODART AS CODIGO,
+             SUM(IIF(F_STO.ALMSTO = 'GEN', F_STO.ACTSTO , 0)) AS GENSTOCK, 
+             SUM(IIF(F_STO.ALMSTO = 'DES', F_STO.ACTSTO , 0)) AS DESSTOCK, 
+             SUM(IIF(F_STO.ALMSTO = 'EXH', F_STO.ACTSTO , 0)) AS EXHSTOCK, 
+             SUM(IIF(F_STO.ALMSTO = 'FDT', F_STO.ACTSTO , 0)) AS FDTSTOCK, 
+             SUM(IIF(F_STO.ALMSTO = 'GEN', F_STO.ACTSTO , 0)  + IIF(F_STO.ALMSTO = 'EXH', F_STO.ACTSTO , 0) ) AS STOCK 
+             FROM F_ART  
+             INNER JOIN F_STO ON F_STO.ARTSTO = F_ART.CODART  
+             WHERE F_STO.ACTSTO <> 0 GROUP BY F_ART.CODART ";
+            $exec = $this->conn->prepare($select);
+            $exec ->execute();
+            $art=$exec->fetchall(\PDO::FETCH_ASSOC);
+            foreach($art as $rt){
+                $produ = DB::table('products')->where('code',$rt["CODIGO"])->VALUE('id');
+                $sto = [
+                    "stock"=>$rt["STOCK"],
+                    "gen"=>$rt["GENSTOCK"],
+                    "exh"=>$rt["EXHSTOCK"],
+                    "des"=>$rt["DESSTOCK"],
+                    "fdt"=>$rt["FDTSTOCK"] 
+                ];
+                DB::table('product_stock')->where('_workpoint', $workpoint)->where('_product',$produ)->update($sto);
+            }
+        }   
+         })->everyThreeMinutes();
     }
 
     /**
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -41,7 +41,35 @@ class ProductController extends Controller
                 return response()->json("Familiarizaciones creadas correctamente");
     }
     public function wor(){
-        $workpoin = env("WORKPOINT");
-        return $workpoin;
+        $workpoint = env("WORKPOINT");
+        if($workpoint == 18){
+            $select = 
+            "SELECT F_ART.CODART AS CODIGO,
+             SUM(IIF(F_STO.ALMSTO = 'GEN', F_STO.ACTSTO , 0)) AS GENSTOCK, 
+             SUM(IIF(F_STO.ALMSTO = 'DES', F_STO.ACTSTO , 0)) AS DESSTOCK, 
+             SUM(IIF(F_STO.ALMSTO = 'EXH', F_STO.ACTSTO , 0)) AS EXHSTOCK, 
+             SUM(IIF(F_STO.ALMSTO = 'FDT', F_STO.ACTSTO , 0)) AS FDTSTOCK, 
+             SUM(IIF(F_STO.ALMSTO = 'GEN', F_STO.ACTSTO , 0)  + IIF(F_STO.ALMSTO = 'EXH', F_STO.ACTSTO , 0) ) AS STOCK 
+             FROM F_ART  
+             INNER JOIN F_STO ON F_STO.ARTSTO = F_ART.CODART  
+             WHERE F_STO.ACTSTO <> 0 GROUP BY F_ART.CODART ";
+            $exec = $this->conn->prepare($select);
+            $exec ->execute();
+            $art=$exec->fetchall(\PDO::FETCH_ASSOC);
+            foreach($art as $rt){
+                $produ = DB::table('products')->where('code',$rt["CODIGO"])->VALUE('id');
+                $sto = [
+                    "stock"=>$rt["STOCK"],
+                    "gen"=>$rt["GENSTOCK"],
+                    "exh"=>$rt["EXHSTOCK"],
+                    "des"=>$rt["DESSTOCK"],
+                    "fdt"=>$rt["FDTSTOCK"] 
+                ];
+                DB::table('product_stock')->where('_workpoint', $workpoint)->where('_product',$produ)->update($sto);
+            }
+
+        return response()->json($sto);    
+            
+        }
     }
 }
